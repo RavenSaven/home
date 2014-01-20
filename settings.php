c22121b67938da264e7b689b4258935a7cf7849e
Ninchen auf Ravermission <savendadraven@gmail.com>
2014-01-20 23:22:54 +0100

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,14 @@
+<?php
+/** 
+   *et up and fix variables und bindet
+ * prozesse and class library ein.
+ 
+ */
+ 
+ define( 'RavenINC', 'includes' );
+
+
+// bindet noch zu installierende sachen ein.
+require( ABSPATH . WPINC . '/load.php' );
+require( ABSPATH . WPINC . '/default-constants.php' );
+
